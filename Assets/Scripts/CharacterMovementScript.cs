e3a959af5764ded0733bdb0cc8e717b785a06a7b
jAbecassis <judicael.abecassis@gmail.com>
2016-12-20 15:40:49 +0100

    [Feature] Character Controller version 1

---
+++ Assets/Scripts/CharacterMovementScript.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class CharacterMovementScript : MonoBehaviour {
+
+    [SerializeField]
+    private float moveSpeed = 10f;
+    [SerializeField]
+    private float jumpForce = 10f;
+
+    private bool isGrounded;
+    private Rigidbody rigid;
+    private SphereCollider footCollider;
+
+    public void Awake()
+    {
+        rigid = GetComponent<Rigidbody>();
+        footCollider = GetComponentInChildren<SphereCollider>();
+    }
+
+    void FixedUpdate()
+    {
+        isGrounded = false;
+        Collider[] colliders = Physics.OverlapSphere(footCollider.transform.position, footCollider.radius);
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if(colliders[i].gameObject.tag != gameObject.tag)
+            {
+                isGrounded = true;
+            }
+        }
+    }
+
+	public void Move(float horizontal, float vertical)
+    {
+        float verticalMove = (isGrounded) ? 0 : vertical;
+        Vector3 direction = new Vector3(horizontal, verticalMove, 0) * moveSpeed * Time.fixedDeltaTime;
+        rigid.MovePosition(rigid.position + direction);
+    }
+
+    public void Jump()
+    {
+        rigid.AddForce(Vector3.up * jumpForce);
+    }
+}
---
+++ Assets/Scripts/PlayerCharacterControlScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CharacterMovementScript))]
+public class PlayerCharacterControlScript : MonoBehaviour {
+
+    CharacterMovementScript characterMovement;
+
+	// Use this for initialization
+	void Awake () {
+        characterMovement = GetComponent<CharacterMovementScript>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        characterMovement.Move(h, v);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            characterMovement.Jump();
+        }
+
+	}
+}
