aef180874d1a2d192c349863dd4299dcc45074ab
jAbecassis <judicael.abecassis@gmail.com>
2016-12-29 17:49:34 +0100

    [Feature] Multiplayer control

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public GameObject PlayerPrefab;
+    public PlayerControllerScript[] players;
+
+	// Use this for initialization
+	void Start () {
+        players = new PlayerControllerScript[4];
+        for (int i = 0; i < players.Length; i++)
+        {
+            GameObject o = Instantiate(PlayerPrefab, new Vector3((float)i, 0f, 0f), Quaternion.identity) as GameObject;
+            players[i] = o.GetComponent<PlayerControllerScript>();
+            players[i].playerId = i;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Scripts/PlayerControllerScript.cs
+++ Assets/Scripts/PlayerControllerScript.cs
@@ -4,6 +4,8 @@ using System.Collections;
 [RequireComponent(typeof(CharacterMovementScript))]
 public class PlayerControllerScript : MonoBehaviour {
 
+    public int playerId = 0;
+
     public delegate void PlayerControlDelegate(float horizontal, float vertical, bool jump);
     public event PlayerControlDelegate PlayerControlEvent;
 
@@ -15,8 +17,13 @@ public class PlayerControllerScript : MonoBehaviour {
     private ConsoleObject actionable;
     private bool isConnected;
 
-	// Use this for initialization
-	void Awake ()
+    private static string HORIZONTAL = "Horizontal_";
+    private static string VERTICAL = "Vertical_";
+    private static string JUMP = "Jump_";
+    private static string ACTION = "Action_";
+
+    // Use this for initialization
+    void Awake ()
     {
         characterMovement = GetComponent<CharacterMovementScript>();
         characterMovement.AddController(this);
@@ -24,12 +31,12 @@ public class PlayerControllerScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
-
+        float h = Input.GetAxis(HORIZONTAL+playerId);
+        float v = Input.GetAxis(VERTICAL+playerId);
+        
         if (PlayerControlEvent != null)
         {
-            PlayerControlEvent(h, v, Input.GetButtonDown("Jump"));
+            PlayerControlEvent(h, v, Input.GetButtonDown(JUMP+playerId));
         }
 
 	}
@@ -38,7 +45,7 @@ public class PlayerControllerScript : MonoBehaviour {
     {
         if (PlayerActionEvent != null)
         {
-            if (Input.GetButtonDown("Fire1"))
+            if (Input.GetButtonDown(ACTION+playerId))
             {
                 PlayerActionEvent();
             }
